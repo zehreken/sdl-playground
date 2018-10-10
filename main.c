f40f4345a0f78a17aee3005b2edddb74b7a72579
zehreken <zehreken@gmail.com>
2018-10-10 23:13:25 +0300

    Update main, have fun

--- main.c
+++ main.c
@@ -4,12 +4,26 @@
 const int WIDTH = 640;
 const int HEIGHT = 480;
 
-void move(SDL_Renderer* renderer, int i)
+struct Vector2
+{
+    float x;
+    float y;};
+
+void drawTriangle(SDL_Renderer* renderer, int i)
 {
     SDL_SetRenderDrawColor(renderer, 255, 0, 0, SDL_ALPHA_OPAQUE);
     SDL_RenderDrawLine(renderer, 320 + i, 200, 300 + i, 240);
     SDL_RenderDrawLine(renderer, 300 + i , 240, 340 + i, 240);
     SDL_RenderDrawLine(renderer, 340 + i, 240, 320 + i, 200);
+}
+
+void drawSquare(SDL_Renderer* renderer, int size, struct Vector2 pos)
+{
+    SDL_SetRenderDrawColor(renderer, 255, 0, 55, SDL_ALPHA_OPAQUE);
+    SDL_RenderDrawLine(renderer, pos.x - size / 2, pos.y - size / 2, pos.x - size / 2, pos.y + size / 2); // left
+    SDL_RenderDrawLine(renderer, pos.x - size / 2, pos.y - size / 2, pos.x + size / 2, pos.y - size / 2); // top
+    SDL_RenderDrawLine(renderer, pos.x + size / 2, pos.y - size / 2, pos.x + size / 2, pos.y + size / 2); // right
+    SDL_RenderDrawLine(renderer, pos.x - size / 2, pos.y + size / 2, pos.x + size / 2, pos.y + size / 2); // bottom
 }
 
 void drawDots(SDL_Renderer* renderer)
@@ -47,8 +61,16 @@ int main(int argc, char* argv[])
                 SDL_RenderDrawLine(renderer, 300, 240, 340, 240);
                 SDL_RenderDrawLine(renderer, 340, 240, 320, 200);
                 
-                move(renderer, i++);
-                if (i > 640) i = 0;
+                drawTriangle(renderer, i++);
+                if (i > WIDTH / 2) i = 0;
+                
+                for (int i = 0; i < 100; i++)
+                {
+                    struct Vector2 pos;
+                    pos.x = (rand() / (RAND_MAX + 1.0)) * WIDTH;
+                    pos.y = (rand() / (RAND_MAX + 1.0)) * HEIGHT;
+                    drawSquare(renderer, 100, pos);
+                }
                 
                 drawDots(renderer);
                 
