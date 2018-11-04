b83a067aeca345e31a4f42f607b9b2313fc24088
zehreken <zehreken@gmail.com>
2018-11-04 14:59:01 +0300

    Fix collision

--- source/primitives.c
+++ source/primitives.c
@@ -15,10 +15,14 @@ void drawBox(SDL_Renderer* renderer, Box2D box2D)
     IntVector2 size = box2D.size;
     ColorRGB color = box2D.color;
     SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, SDL_ALPHA_OPAQUE);
-    SDL_RenderDrawLine(renderer, pos.x - size.x / 2, pos.y - size.y / 2, pos.x - size.x / 2, pos.y + size.y / 2); // left
-    SDL_RenderDrawLine(renderer, pos.x - size.x / 2, pos.y - size.y / 2, pos.x + size.x / 2, pos.y - size.y / 2); // top
-    SDL_RenderDrawLine(renderer, pos.x + size.x / 2, pos.y - size.y / 2, pos.x + size.x / 2, pos.y + size.y / 2); // right
-    SDL_RenderDrawLine(renderer, pos.x - size.x / 2, pos.y + size.y / 2, pos.x + size.x / 2, pos.y + size.y / 2); // bottom
+//    SDL_RenderDrawLine(renderer, pos.x - size.x / 2, pos.y - size.y / 2, pos.x - size.x / 2, pos.y + size.y / 2); // left
+//    SDL_RenderDrawLine(renderer, pos.x - size.x / 2, pos.y - size.y / 2, pos.x + size.x / 2, pos.y - size.y / 2); // top
+//    SDL_RenderDrawLine(renderer, pos.x + size.x / 2, pos.y - size.y / 2, pos.x + size.x / 2, pos.y + size.y / 2); // right
+//    SDL_RenderDrawLine(renderer, pos.x - size.x / 2, pos.y + size.y / 2, pos.x + size.x / 2, pos.y + size.y / 2); // bottom
+    SDL_RenderDrawLine(renderer, pos.x, pos.y, pos.x, pos.y + size.y);
+    SDL_RenderDrawLine(renderer, pos.x, pos.y, pos.x + size.x, pos.y);
+    SDL_RenderDrawLine(renderer, pos.x + size.x, pos.y, pos.x + size.x, pos.y + size.y);
+    SDL_RenderDrawLine(renderer, pos.x, pos.y + size.y, pos.x + size.x, pos.y + size.y);
 }
 
 void drawDots(SDL_Renderer* renderer)
