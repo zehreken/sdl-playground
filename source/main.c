033e18d8fdbda5bd665f03e70b05627a1aa93573
zehreken <zehreken@gmail.com>
2019-03-10 20:35:49 +0100

    Clean up

--- source/main.c
+++ source/main.c
@@ -39,9 +39,9 @@ int main(int argc, char* argv[])
     triangle.position.x = 0;
     triangle.position.y = 0;
     triangle.size = 10;
-    triangle.color.r = 0;
+    triangle.color.r = 255;
     triangle.color.g = 0;
-    triangle.color.b = 0;
+    triangle.color.b = 255;
     
     if (SDL_Init(SDL_INIT_VIDEO) == 0)
     {
@@ -98,14 +98,14 @@ int main(int argc, char* argv[])
                 SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
                 SDL_RenderClear(renderer);
                 
-                SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
-                SDL_RenderDrawLine(renderer, 320, 200, 300, 240);
-                SDL_RenderDrawLine(renderer, 300, 240, 340, 240);
-                SDL_RenderDrawLine(renderer, 340, 240, 320, 200);
-                
                 drawTriangle(renderer, i++);
                 drawTriangle2(renderer, triangle);
-                if (i > WIDTH / 2) i = 0;
+                triangle.position.x += 1;
+                if (i > WIDTH / 2)
+                {
+                    i = 0;
+                    triangle.position.x = 0;
+                }
                 
 //                drawBox(renderer, a);
 //                drawBox(renderer, b);
