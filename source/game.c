003cda679dd5391c62543d6264763747e6a910f2
zehreken <zehreken@gmail.com>
2019-03-20 21:04:53 +0100

    Create gameObjects

--- source/game.c
+++ source/game.c
@@ -1,4 +1,19 @@
 #include "game.h"
+#include "gameObject.h"
+#include "constants.h"
+
+const int GAME_OBJECT_COUNT = 100;
+GameObject gameObjects[GAME_OBJECT_COUNT];
+
+void init()
+{
+    for (int i = 0; i < GAME_OBJECT_COUNT; i++)
+    {
+        gameObjects[i].position.x = rand() / (RAND_MAX + 1.0) * WIDTH;
+        gameObjects[i].position.y = rand() / (RAND_MAX + 1.0) * HEIGHT;
+        gameObjects[i].boxSize.x = gameObjects[i].boxSize.y = 10 *  (rand() / (RAND_MAX + 1.0)) * 90;
+    }
+}
 
 void update()
 {
