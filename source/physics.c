55d5a6efe58821b8d6f145bc91929d585af9d893
zehreken <zehreken@gmail.com>
2019-04-20 20:50:28 +0200

    Simplify physics

--- source/physics.c
+++ source/physics.c
@@ -1,34 +1,5 @@
 #include "physics.h"
 
-static void solveCollision(Box2D* a, Box2D* b)
-{
-    if (a->position.x < b->position.x + b->size.x &&
-        a->position.x + a->size.x > b->position.x &&
-        a->position.y < b->position.y + b->size.y &&
-        a->position.y + a->size.y > b->position.y)
-    {
-        // There is collision
-        a->hasCollision = true;
-        b->hasCollision = true;
-    }
-    else
-    {
-        b->hasCollision = false;
-    }
-}
-
-void solvePhysics(Box2D boxes[], int size)
-{
-    boxes[0].hasCollision = false;
-    for (int i = 0; i < 1; i++)
-    {
-        for (int j = i + 1; j < size; j++)
-        {
-            solveCollision(&boxes[i], &boxes[j]);
-        }
-    }
-}
-
 void solveCollisionGame(GameObject* a, GameObject* b)
 {
     if (a->position.x < b->position.x + b->rect.w &&
