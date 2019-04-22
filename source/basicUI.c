3d76b68408c58d74b73290fa59f15264f0fe121f
zehreken <zehreken@gmail.com>
2019-04-22 20:57:13 +0200

    Fix memory leak, always free SDL_Surface and SDL_Texture

--- source/basicUI.c
+++ source/basicUI.c
@@ -1,12 +1,23 @@
 #include "basicUI.h"
 #include "SDL2_ttf/SDL_ttf.h"
 #include "timeUtils.h"
+#include "constants.h"
+#include "playerShip.h"
 
+static char c[50];
+static char b[11] = " totalTime";
 static TTF_Font* font;
 static SDL_Surface* surface;
 static SDL_Texture* texture;
 static SDL_Rect textRect;
-static SDL_Color color = { 255, 0, 255, 255 };
+static SDL_Color color = { 255, 255, 255, 255 };
+
+static SDL_Surface* playerHitPointSurface;
+static SDL_Texture* playerHitPointTexture;
+static SDL_Rect playerHitPointRect;
+
+static const float UI_UPDATE_PERIOD = 1;
+static float updateTimer = 0;
 
 void initBasicUI(SDL_Renderer* renderer)
 {
@@ -16,6 +27,7 @@ void initBasicUI(SDL_Renderer* renderer)
     
     surface = TTF_RenderText_Solid(font, "testing testing", color);
     texture = SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_FreeSurface(surface);
     textRect.x = textRect.y = 0;
     SDL_QueryTexture(texture, NULL, NULL, &textRect.w, &textRect.h);
 }
@@ -23,15 +35,30 @@ void initBasicUI(SDL_Renderer* renderer)
 void updateBasicUI(SDL_Renderer* renderer)
 {
     float deltaTime = getDeltaTimeInSeconds();
+    updateTimer += deltaTime;
     
-//    float f = getAverageFps();
-    char c[50];
-    char b[11] = " totalTime";
-    sprintf(c, "%g", deltaTime);
-    strcat(c, b);
-    surface = TTF_RenderText_Solid(font, c, color);
-    texture = SDL_CreateTextureFromSurface(renderer, surface);
-    SDL_QueryTexture(texture, NULL, NULL, &textRect.w, &textRect.h);
+    if (updateTimer >= UI_UPDATE_PERIOD)
+    {
+        updateTimer -= UI_UPDATE_PERIOD;
+//        float f = getAverageFps();
+    
+        sprintf(c, "DeltaTime: %f", deltaTime);
+//    puts(c);
+//    strcat(c, b);
+        
+        // Free old texture
+        SDL_DestroyTexture(texture);
+        surface = TTF_RenderText_Solid(font, c, color);
+        texture = SDL_CreateTextureFromSurface(renderer, surface);
+        SDL_FreeSurface(surface);
+        SDL_QueryTexture(texture, NULL, NULL, &textRect.w, &textRect.h);
+    
+//    playerHitPointSurface = TTF_RenderText_Solid(font, getPlayerHitPoint(), color);
+//    playerHitPointTexture = SDL_CreateTextureFromSurface(renderer, playerHitPointSurface);
+//    SDL_QueryTexture(playerHitPointTexture, NULL, NULL, &playerHitPointRect.w, &playerHitPointRect.h);
+//    playerHitPointRect.x = WIDTH - playerHitPointRect.w;
+//    playerHitPointRect.y = HEIGHT - playerHitPointRect.h;
+    }
     
     SDL_RenderCopy(renderer, texture, NULL, &textRect);
 }
