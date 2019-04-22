1c0cf18696a6c724853dd8322c1ad9fecb5f3849
zehreken <zehreken@gmail.com>
2019-04-22 21:44:37 +0200

    Add score and hp displays

--- source/basicUI.c
+++ source/basicUI.c
@@ -3,13 +3,14 @@
 #include "timeUtils.h"
 #include "constants.h"
 #include "playerShip.h"
+#include "scoreController.h"
 
-static char c[50];
-static char b[11] = " totalTime";
+static char scoreString[20];
+static char hpString[7];
 static TTF_Font* font;
-static SDL_Surface* surface;
-static SDL_Texture* texture;
-static SDL_Rect textRect;
+static SDL_Surface* scoreSurface;
+static SDL_Texture* scoreTexture;
+static SDL_Rect scoreRect;
 static SDL_Color color = { 255, 255, 255, 255 };
 
 static SDL_Surface* playerHitPointSurface;
@@ -17,19 +18,13 @@ static SDL_Texture* playerHitPointTexture;
 static SDL_Rect playerHitPointRect;
 
 static const float UI_UPDATE_PERIOD = 1;
-static float updateTimer = 0;
+static float updateTimer = 1;
 
 void initBasicUI(SDL_Renderer* renderer)
 {
     font = TTF_OpenFont("fonts/emulogic.ttf", 20);
     if (!font)
         printf("Font loading error");
-    
-    surface = TTF_RenderText_Solid(font, "testing testing", color);
-    texture = SDL_CreateTextureFromSurface(renderer, surface);
-    SDL_FreeSurface(surface);
-    textRect.x = textRect.y = 0;
-    SDL_QueryTexture(texture, NULL, NULL, &textRect.w, &textRect.h);
 }
 
 void updateBasicUI(SDL_Renderer* renderer)
@@ -40,27 +35,32 @@ void updateBasicUI(SDL_Renderer* renderer)
     if (updateTimer >= UI_UPDATE_PERIOD)
     {
         updateTimer -= UI_UPDATE_PERIOD;
-//        float f = getAverageFps();
     
-        sprintf(c, "DeltaTime: %f", deltaTime);
-//    puts(c);
-//    strcat(c, b);
+//        puts(c);
+//        strcat(c, b);
         
+        sprintf(scoreString, "SCORE:%d", getScore());
         // Free old texture
-        SDL_DestroyTexture(texture);
-        surface = TTF_RenderText_Solid(font, c, color);
-        texture = SDL_CreateTextureFromSurface(renderer, surface);
-        SDL_FreeSurface(surface);
-        SDL_QueryTexture(texture, NULL, NULL, &textRect.w, &textRect.h);
-    
-//    playerHitPointSurface = TTF_RenderText_Solid(font, getPlayerHitPoint(), color);
-//    playerHitPointTexture = SDL_CreateTextureFromSurface(renderer, playerHitPointSurface);
-//    SDL_QueryTexture(playerHitPointTexture, NULL, NULL, &playerHitPointRect.w, &playerHitPointRect.h);
-//    playerHitPointRect.x = WIDTH - playerHitPointRect.w;
-//    playerHitPointRect.y = HEIGHT - playerHitPointRect.h;
+        SDL_DestroyTexture(scoreTexture);
+        scoreSurface = TTF_RenderText_Solid(font, scoreString, color);
+        scoreTexture = SDL_CreateTextureFromSurface(renderer, scoreSurface);
+        // Free old surface
+        SDL_FreeSurface(scoreSurface);
+        SDL_QueryTexture(scoreTexture, NULL, NULL, &scoreRect.w, &scoreRect.h);
+        
+        sprintf(hpString, "HP:%d", getPlayerShipHitPoint());
+        // Free old texture
+        SDL_DestroyTexture(playerHitPointTexture);
+        playerHitPointSurface = TTF_RenderText_Solid(font, hpString, color);
+        playerHitPointTexture = SDL_CreateTextureFromSurface(renderer, playerHitPointSurface);
+        // Free old surface
+        SDL_FreeSurface(playerHitPointSurface);
+        SDL_QueryTexture(playerHitPointTexture, NULL, NULL, &playerHitPointRect.w, &playerHitPointRect.h);
+        playerHitPointRect.x = WIDTH - playerHitPointRect.w;
     }
     
-    SDL_RenderCopy(renderer, texture, NULL, &textRect);
+    SDL_RenderCopy(renderer, scoreTexture, NULL, &scoreRect);
+    SDL_RenderCopy(renderer, playerHitPointTexture, NULL, &playerHitPointRect);
 }
 
 void quitBasicUI()
